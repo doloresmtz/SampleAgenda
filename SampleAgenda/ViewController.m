47ef17b69b2bedcd745605102010464152a9a2b4
“Dolores <doloresmtz@gmail.com>
2015-01-17 23:23:37 -0600

    ViewControler Basico con dos botones y un array de imágenes en carrusel

--- SampleAgenda/ViewController.m
+++ SampleAgenda/ViewController.m
@@ -8,7 +8,11 @@
 
 #import "ViewController.h"
 
+NSMutableArray  *imagesArray;
+int iImage = 0;
+
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIImageView *imgIntro;
 
 @end
 
@@ -18,12 +22,32 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    imagesArray = [[NSMutableArray alloc] initWithObjects:@"march.gif", @"magy.jpg", @"lissa.jpg", @"homero.jpg", @"bar.gif", nil];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+- (IBAction)moveLeft:(id)sender {
+    if (iImage > 0){
+        iImage--;
+    }
+    else{
+        iImage = 4;
+    }
+    self.imgIntro.image = [UIImage imageNamed:imagesArray[iImage]];
+}
+- (IBAction)moveRigh:(id)sender {
+    if (iImage >= 4){
+        iImage = 0;
+    }else{
+        iImage++;
+    }
+   self.imgIntro.image = [UIImage imageNamed:imagesArray[iImage]];
+}
+- (IBAction)goVer:(id)sender {
 }
 
 @end
