eae1cf45dfe513e90fa0416a7af0bcadcc2fbba1
“Dolores <doloresmtz@gmail.com>
2015-01-17 22:49:24 -0600

    Initial Commit

---
+++ SampleAgenda/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SampleAgenda
+//
+//  Created by SantiagoDls on 17/01/15.
+//  Copyright (c) 2015 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
